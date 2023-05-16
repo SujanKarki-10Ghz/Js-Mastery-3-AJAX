2ae45f574026e34f18e00aabb7462ae85e50ad18
Sujan Karki <83892476+Apocalypse526@users.noreply.github.com>
2023-05-16 21:17:49 +0545

    callbackhell and pyramid of doom

--- callback.js
+++ callback.js
@@ -57,4 +57,113 @@
   }, ()=>{
     console.log("Wrong data type");
     console.log("Please enter numbers only");
-  });   
+  });
+
+  //callbacks, callback hell, pyramid of doom
+
+  const heading1= document.querySelector(".heading1");
+  const heading2 = document.querySelector(".heading2");
+  const heading3 = document.querySelector(".heading3");
+  const heading4 = document.querySelector(".heading4");
+  const heading5 = document.querySelector(".heading5");
+  const heading6 = document.querySelector(".heading6");
+  const heading7 = document.querySelector(".heading7");
+  // setTimeout(() => {
+  //   heading1.textContent = "Heading1";
+  //   heading1.style.color = "violet";
+  // }, 1000);
+
+  // setTimeout(()=>{
+  //   heading2.textContent = "Heading2";
+  //   heading2.style.color = "purple";
+  // }, 2000);
+
+  //rather
+
+  // setTimeout(()=>{
+  //   heading1.textContent = "Heading1";
+  //   heading1.style.color = "violet";
+
+  //   setTimeout(()=>{
+  //     heading2.textContent = "Heading2";
+  //     heading2.style.color = "red"; 
+  //   }, 1000)
+  // },1000)
+
+
+/**task => callback hell*/
+
+// text     Delay   Color
+// one       1s       Violet
+// Two       2s       Purple
+// Three     2s       Red
+// Four      1s       Pink
+// Five      2s       Green
+// Six       3s       Blue
+// Seven     1s       Brown
+
+
+// setTimeout(()=>{
+//   heading1.textContent = "one";
+//   heading1.style.color = "violet";
+//   setTimeout(()=>{
+//     heading2.textContent = "Two";
+//     heading2.style.color = "purple";
+    
+//     setTimeout(()=>{
+//       heading3.textContent = "Three";
+//       heading3.style.color = "red";
+
+//       setTimeout(()=>{
+//         heading4.textContent = "Four";
+//         heading4.style.color = "pink";
+
+//         setTimeout(()=>{
+//           heading5.textContent= "Five";
+//           heading5.style.color = "green";
+
+//           setTimeout(() => {
+//             heading6.textContent = "Six";
+//             heading6.style.color = "blue";
+
+//             setTimeout(() => {
+//               heading7.textContent="Seven";
+//               heading7.style.color = "brown";
+//             }, 1000);
+//           }, 3000);
+//         },2000)
+//       },1000)
+//     },2000)
+//   }, 2000)
+// },1000)
+
+
+function changeText(element, text, color, time, onSuccessCallback, onFailureCallback){
+  setTimeout(()=>{
+    if(onSuccessCallback){
+      onSuccessCallback();
+    }
+    if(onFailureCallback){
+      onFailureCallback();
+    }
+    element.textContent = text;
+    element.style.color = color;
+  }, time)
+}
+
+//pyramid of doom
+changeText(heading1, "one", "violet", 1000, ()=>{
+  changeText(heading2, "two", "purple", 2000, ()=>{
+    changeText(heading3, "three", "red", 2000, ()=>{
+      changeText(heading4, "four", "pink", 1000, ()=>{
+        changeText(heading5, "five", "green", 2000, ()=>{
+          changeText(heading6, "six", "blue", 3000, ()=>{
+            changeText(heading7, "seven", "brown", 1000, ()=>{
+  
+            }, ()=>{console.log("heading7 doesn't exist");})
+          }, ()=>{console.log("heading6 doesn't exist");})
+        }, ()=>{console.log("heading5 doesn't exist");})
+      }, ()=>{console.log("heading4 doesn't exist");})
+    }, ()=>{console.log("heading3 doesn't exist");})
+  }, ()=>{console.log("heading2 doesn't exist");})
+}, ()=>{console.log("heading1 doesn't exist");})
