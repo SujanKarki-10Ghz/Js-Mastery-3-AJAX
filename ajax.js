a1af6527385d106fe6b013f45b295450a3e4488a
Sujan Karki <83892476+Apocalypse526@users.noreply.github.com>
2023-05-18 20:06:36 +0545

    ajax intro, xhrHTTPRequest

---
+++ ajax.js
@@ -0,0 +1,16 @@
+/**Basic Theory
+ * AJAX = Asynchronous JavaScript and XML
+ * is a set of "web development techniques" using many web technologies on the "client-side"
+ * to create asynchronous web applications.
+ * With AJAX, web applications can send and retrieve data from a server asychronously (in the background)
+ * without interfering with the display and behavior of exisiting page
+ */
+
+// We don't use data in XML format anymore
+// We use JSON now 
+
+
+//we have three common ways to create and send request to the server
+// 1. XmlHTTPRequest (old way of doing)
+// 2. fetch API (new way of doing)
+// 3. axios (this is third party library)
---
+++ xhr.js
@@ -0,0 +1,19 @@
+const URL = "https://jsonplaceholder.typicode.com/posts";
+const xhr = new XMLHttpRequest();
+xhr.open("GET", URL);
+//xhr.onreadystatechange = function(){
+    // console.log(xhr.readyState);
+    // if(xhr.readyState === 4){
+    //     const response = xhr.response;
+    //     const data = JSON.parse(response);
+    //     console.log(data);
+    // }
+//}
+
+xhr.onload = function(){
+    const response = xhr.response;
+    const data = JSON.parse(response);
+    console.log(data);
+}
+
+xhr.send();
